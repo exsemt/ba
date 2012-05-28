7ca9139b585933b282989003460b2bad4f1f8fe0
exsemt <exsemt@gmail.com>
2012-05-28 14:10:36 +0200

    show average data from scenarios

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,10 @@
 class DashboardController < ApplicationController
+
   def index
+    @sql = SqlRequest.select(['name', 'table_size AS "StarFact table size"', 'avg(sql_duration)', 'GROUP_CONCAT(sql_duration)', 'sql_query']).
+      group('name', 'table_size').order(:table_size).to_sql
+
+    @db_response = ActiveRecord::Base.connection.select(@sql)
   end
+
 end
