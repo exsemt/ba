214a67c1acdbdfaf47e5145bdcd7cfeb9acf5ea8
exsemt <exsemt@gmail.com>
2012-05-28 20:40:12 +0200

    add chart to dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,10 +1,13 @@
 class DashboardController < ApplicationController
 
   def index
-    @sql = SqlRequest.select(['name', 'table_size AS "StarFact table size"', 'avg(sql_duration)', 'GROUP_CONCAT(sql_duration)', 'sql_query']).
+    @sql = SqlRequest.select(['name', 'table_size AS "StarFact table size"', 'avg(sql_duration)', 'GROUP_CONCAT(sql_duration)', 'substr(sql_query,1,50)']).
       group('name', 'table_size').order(:table_size).to_sql
 
     @db_response = ActiveRecord::Base.connection.select(@sql)
+
+    @visualize_chart = SqlRequest.select(['name', 'table_size', 'avg(sql_duration) AS avg', 'GROUP_CONCAT(sql_duration)']).
+      group('name', 'table_size').order(:table_size).group_by(&:table_size)
   end
 
 end
