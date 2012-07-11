a45ef1739037b501b60392a92553a09d333c53c1
exsemt <exsemt@gmail.com>
2012-07-11 22:03:25 +0200

    add charts "area" to dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -6,8 +6,11 @@ class DashboardController < ApplicationController
 
     @db_response = ActiveRecord::Base.connection.select(@sql)
 
-    @visualize_chart = SqlRequest.select(['name', 'table_size', 'avg(sql_duration) AS avg', 'GROUP_CONCAT(sql_duration)']).
+    @visualize_chart_bar = SqlRequest.select(['name', 'table_size', 'avg(sql_duration) AS avg', 'GROUP_CONCAT(sql_duration)']).
       group('name', 'table_size').order(:table_size).group_by(&:table_size)
+
+    @visualize_chart_area = SqlRequest.select(['name', 'scenario', 'table_size', 'avg(sql_duration) AS avg', 'GROUP_CONCAT(sql_duration)']).
+      group('name', 'scenario', 'table_size').order(:scenario, :table_size).group_by(&:scenario)
   end
 
 end
